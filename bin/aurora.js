a66e016dc3e12cb01c79a59279549da513941ba3
Edward <wang.huiyang@outlook.com>
2021-01-14 09:27:04 +0800

    Fix: -h, --help

--- bin/aurora.js
+++ bin/aurora.js
@@ -6,19 +6,20 @@ const chalk = require('chalk')
 program
   .version('1.0.0', '-v --version')
   .usage('<command> [name]')
-  .command('init', 'create a new project')
+  .command('api', 'create a api')
   .command('comp', 'create a component')
+  .command('init', 'create a new project')
   .command('page', 'create a page')
   .command('store', 'create a store')
-  .command('api', 'create a api')
 
 program.on('--help', () => {
   console.log('')
   console.log(chalk.green('Examples:'))
-  console.log(chalk.green('  $aurora init demo'))
-  console.log(chalk.green('  $aurora comp Header'))
-  console.log(chalk.green('  $aurora page Login [-o]'))
   console.log(chalk.green('  $aurora api Common'))
+  console.log(chalk.green('  $aurora comp Header'))
+  console.log(chalk.green('  $aurora init demo'))
+  console.log(chalk.green('  $aurora page Login [-o]'))
+  console.log(chalk.green('  $aurora store common'));
 })
 
 program.parse(process.argv)
